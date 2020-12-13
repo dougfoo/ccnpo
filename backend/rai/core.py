90c7f34d15399bf66cbaaaf6694883f4ed4e9c6f
dougfoo <doug.cha@gmail.com>
2020-12-13 18:26:14 +0900

    bootstrap py package struct for resume-ai (rai) package

---
+++ backend/rai/core.py
@@ -0,0 +1,12 @@
+
+class Resume(object):
+
+    def __init__(self):
+        self.attribs = {}
+        pass
+
+    def summary(self):
+        pass
+
+    def add_attrib(self, name, value):
+        pass
