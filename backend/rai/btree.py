deb2787c721262c340a49ccbb387b7eaec9d6c92
dougfoo <doug.cha@gmail.com>
2020-12-19 18:09:39 +0900

    bree

--- backend/rai/btree.py
+++ backend/rai/btree.py
@@ -1,10 +1,39 @@
 # balancing binary tree
 
-def printInorder(root):
+# bfs
+import collections
+
+def printPretty(root):
+    if (root == None):
+        return
+    # print(root.node, end='')
     if (root.left):
-        printInorder(root.left)
+        print(root.left.node, end='')
     if (root.right):
-        printInorder(root.right)
+        print(f' {root.right.node}', end='')
+    print('\n', end='')
+    printPretty(root.left)
+    printPretty(root.right)
+
+def printInOrder(root):
+    if (root.left):
+        printInOrder(root.left)
+    print(root.node)
+    if (root.right):
+        printInOrder(root.right)
+
+def printPreOrder(root):
+    print(root.node)
+    if (root.left):
+        printPreOrder(root.left)
+    if (root.right):
+        printPreOrder(root.right)
+
+def printPostOrder(root):
+    if (root.left):
+        printPostOrder(root.left)
+    if (root.right):
+        printPostOrder(root.right)
     print(root.node)
 
 class Node(object):
@@ -14,7 +43,7 @@ class Node(object):
         self.right=right
 
     def insert(self, node):
-        if (node <= self.node):
+        if (node.node <= self.node):
             if (self.left is None):
                 self.left = node
             else:
@@ -26,16 +55,82 @@ class Node(object):
                 self.right.insert(node)
 
     def find(self, key):
+        n = self.findNode(key)
+        if (n == None):
+            return -1
+        else:
+            return n.node # the value
+
+    def findNode(self, key):
         if (key == self.node):
-            return self.node
+            return self
         elif (key < self.node):
             if (self.left is None):
-                return -1
+                return None
             else:
-                return self.left.find(key)
+                return self.left.findNode(key)
         else:
             if (self.right is None):
-                return -1
+                return None
             else:
-                return self.right.find(key)
+                return self.right.findNode(key)
+
+    def delete(self, key):
+        n = self.find(key)
+        if (n == None):
+            return -1
+        else:
+           pass
+    #     4
+    #  2   5
+    # 1 3   6 
+    # delete (2) - if 2 child u can do either left or right up
+    #     4              4
+    #  1   5     or    3   5
+    #   3   6         1     6
+    # delete (5) - if one child, then replace / move up
+    #     4
+    #  2   6
+    # 1 3    
+    # rebuild entire subtree?  or in general you replace
+    # the deleted w/ one subchild if exists is all we need to do
+    # as subchildren still honor tree rule
+
+    def balance(self):
+        pass
+    # rebalance tree from :
+    #   1              2
+    #     2               3
+    #       3           1
+    #  to             to 
+    #   2              2 
+    #  1  3          1   3
+    # how to do this... ?? key is equal l/r at least sub (or just at top
+    # is the most important for median calc)
+
+n = Node(6,None, None)
+n.insert(Node(1,None,None))
+n.insert(Node(5,None,None))
+n.insert(Node(7,None,None))
+n.insert(Node(2,None,None))
+n.insert(Node(4,None,None))
+n.insert(Node(3,None,None))
+
+# 6,1,5,7,2,4,3 ->
+#      6
+#   1    7
+#     5
+#   2    
+#     4
+#   3
+
+print('inorder')
+printInOrder(n)
+print('preorder')
+printPreOrder(n)
+print('postorder')
+printPostOrder(n)
+print('pretty')
+print(n.node)
+printPretty(n)
 
--- backend/testing/test_rai.py
+++ backend/testing/test_rai.py
@@ -13,3 +13,42 @@ def test_insert():
 
     assert (n.node == 1)
     assert (n.right.node == 2)
+
+def setup():
+    n = Node(6,None, None)
+    n.insert(Node(1,None,None))
+    n.insert(Node(5,None,None))
+    n.insert(Node(7,None,None))
+    n.insert(Node(2,None,None))
+    n.insert(Node(4,None,None))
+    n.insert(Node(3,None,None))
+    return n
+
+def setup2():
+    n = Node(4,None, None)
+    n.insert(Node(2,None,None))
+    n.insert(Node(1,None,None))
+    n.insert(Node(3,None,None))
+    n.insert(Node(5,None,None))
+    n.insert(Node(6,None,None))
+    return n
+
+def test_find():
+    n = setup()
+    pos = n.find(4)
+    assert (pos != -1)
+    pos = n.find(8)
+    assert (pos == -1)
+
+# def test_delete():
+#     n = setup2()
+#     n.delete(2)
+#     assert(n.find(2) == -1)
+
+# 6,1,5,7,2,4,3 ->
+#      6
+#   1    7
+#     5
+#   2    
+#     4
+#   3
